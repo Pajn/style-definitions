d176046eb41a919abbfc29efe233b6ea75050c8b
Rasmus Eneman <rasmus@eneman.eu>
2017-06-23 00:16:46 +0200

    Initial commit

---
+++ src/layout.ts
@@ -0,0 +1,78 @@
+import {CSSProperties} from 'glamorous'
+
+export const flex = (
+  flex: number | boolean | {grow?: number; shrink?: number},
+): CSSProperties => ({
+  flexGrow: flex === true
+    ? 1
+    : flex === false
+      ? undefined
+      : flex === undefined
+        ? undefined
+        : typeof flex === 'number' ? flex : flex.grow,
+  flexShrink: flex === true
+    ? undefined
+    : flex === false
+      ? undefined
+      : flex === undefined
+        ? undefined
+        : typeof flex === 'number' ? flex : flex.grow,
+})
+
+export const column = ({
+  horizontal,
+  vertical,
+  self,
+  reverse,
+  flex: flex_,
+}: {
+  horizontal?: 'center' | 'flex-start' | 'flex-end' | 'stretch' | 'baseline'
+  vertical?:
+    | 'center'
+    | 'flex-start'
+    | 'flex-end'
+    | 'space-between'
+    | 'space-around'
+  self?: 'center' | 'flex-start' | 'flex-end' | 'stretch'
+  reverse?: boolean
+  flex?: number | boolean | {grow?: number; shrink?: number}
+}): CSSProperties => ({
+  display: 'flex',
+  flexDirection: reverse ? 'column' : 'column-reverse',
+  alignItems: horizontal,
+  justifyContent: vertical,
+  self,
+  ...flex_ === undefined ? undefined : flex(flex_),
+})
+
+export const row = ({
+  horizontal,
+  vertical,
+  self,
+  reverse,
+  flex: flex_,
+}: {
+  horizontal?:
+    | 'center'
+    | 'flex-start'
+    | 'flex-end'
+    | 'space-between'
+    | 'space-around'
+  vertical?: 'center' | 'flex-start' | 'flex-end' | 'stretch' | 'baseline'
+  self?: 'center' | 'flex-start' | 'flex-end' | 'stretch'
+  reverse?: boolean
+  flex?: number | boolean | {grow?: number; shrink?: number}
+}): CSSProperties => ({
+  display: 'flex',
+  flexDirection: reverse ? 'row' : 'row-reverse',
+  alignItems: vertical,
+  justifyContent: horizontal,
+  self,
+  ...flex_ === undefined ? undefined : flex(flex_),
+})
+
+export const a = (): CSSProperties => ({})
+
+export const b = ({withA = false}): CSSProperties => ({
+  ...withA ? a() : undefined,
+})
---
+++ src/typography.ts
@@ -0,0 +1,5 @@
+export const ellipsis = {
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap',
+}
